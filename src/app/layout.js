4b38edee108577636bf9638750ac9b329762d05d
Sefa Eyer <eyersefa@gmail.com>
2024-10-17 22:37:20 +0300

    v1.17

---
+++ src/app/layout.js
@@ -0,0 +1,26 @@
+import { MainMenubar } from "@/components/common/header/main-menubar";
+import { Topbar } from "@/components/common/header/topbar";
+import { config } from "@/helpers/config";
+import { montserrat } from "@/helpers/fonts";
+import "@/styles/index.scss";
+
+
+export const metadata = {
+  title: {
+    template: `%s | ${config.project.name}`,
+    default: `${config.project.name} - ${config.project.slogan}`,
+  },
+  description: config.project.description
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={montserrat.variable}>
+      <body>
+        <Topbar/>
+        <MainMenubar/>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Page = () => {
+	return <div>Homepage
+
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+	</div>;
+};
+
+export default Page;
